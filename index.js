ed0a48b2fa6e888ea226e7486e874481bb6327c1
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-02 17:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,9 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
 if (katzDeliLine>[]) {
-return `The line is currently: ${katzDeliLine[]. ${name}, `;
+return `The line is currently: ${katzDeliLine.length}. ${name}, `;
 } else { 
   return `The line is currently empty.`; }
 }
 
-var line=[];
-
-for(var i=0;i<katzDeliLine.length;i++) {
-      line.push(` ${i+1}. ${katzDeliLine[i]}`);
-    }
 
