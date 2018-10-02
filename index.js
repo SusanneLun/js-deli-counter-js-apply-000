5f08b4a5a7627fef970fe99e1bfe7dab57d0eca1
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-02 17:30:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine>[]) {
+  return `Currently serving ${katzDeliLine.shift()}.`
+  }
+ else {
+  return `There is nobody waiting to be served!`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+
+if (katzDeliLine>[]) {
+return `The line is currently: ${katzDeliLine[n]}. ${name}, `;
+} else { 
+  return `The line is currently empty.`; }
+}
+
+___________________________________________________________
+var line = []
+
+for(var i=0;i<katzDeliLine.length;i++) {
+      line.push(` ${i+1}. ${katzDeliLine[i]}`);
+    }
+return "The line is currently:" + line;
